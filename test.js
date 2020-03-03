13d54ed40242c1ed74cbab0e0301f78eb2a6ab47
leeingyu <krpeppermint100@gmail.com>
2020-03-03 15:21:05 +0900

    gitignore

--- test.js
+++
@@ -1,9 +0,0 @@
-function outFunc(outVar) {
-  return function inFunc(inVar) {
-    console.log(" outVar : ", outVar);
-    console.log(" inVar: ", inVar);
-  };
-}
-
-const out = outFunc("outVar");
-out("inVar");

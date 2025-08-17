f0f0fb8f52ef8d029a5bb695fba1eba4624e341b
peppermint100 <krpeppermint100@gmail.com>
2025-08-17 10:16:39 +0900

    security group, key pair

--- terraform/terraform_registry_module/instance.tf
+++ terraform/terraform_registry_module/instance.tf
@@ -6,4 +6,10 @@ resource "aws_instance" "example_server" {
   ami           = "ami-0dd97ebb907cf9366"
   instance_type = "t2.micro"
   subnet_id = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+}
+
+resource "aws_key_pair" "mykey" {
+  key_name = "mykey"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPAbK6iG91bObvFieLYaOj2WOmvvE5E7BAxYEc2jni1M peppermint100@PepperMac.local"
 }
